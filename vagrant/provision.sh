771e0704bbba24ff55f1cf94a74c875ef7898746
adrrf <adrromflo@alum.us.es>
2023-11-30 10:31:26 +0100

    feat: vagrant

---
+++ vagrant/provision.sh
@@ -0,0 +1,12 @@
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install -y git python3 python3-pip screen 
+
+git clone https://github.com/adrrf/EGC-2324-turno21-adrromflo.git
+
+cd 1920-Practica-1
+
+pip3 install -r requirements.txt 
+
